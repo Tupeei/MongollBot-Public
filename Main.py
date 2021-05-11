b1ac5913c11e5f530a40f1a1b80da66e47274310
Tupei <49803918+Tupeei@users.noreply.github.com>
2021-05-11 10:33:26 +0200

    Main file
    Python

---
+++ Main.py
@@ -0,0 +1,184 @@
+import random
+import discord
+import datetime
+from discord.ext import commands, tasks
+from itertools import cycle
+import os
+
+client = commands.Bot(command_prefix=".")
+status = cycle([".help"])
+
+client.remove_command("help")
+foot = "\".help\" para todos los comandos"
+TOKEN = "ODMyMzc3Mzg0MTQxMDYyMTc0.YHi52Q.lh1jhO6CJhpPO9M5XYWr8khTZqs"
+
+# Startup
+@client.event
+async def on_ready():
+    change_status.start()
+    print("Bot ready!")
+
+
+@tasks.loop(seconds=15)
+async def change_status():
+    await client.change_presence(activity=discord.Activity(
+        type=discord.ActivityType.watching, name=(next(status))))
+
+# EasterEggs
+# joya
+@client.command()
+async def joya(ctx, member: discord.Member):
+    await ctx.channel.purge(limit=1)
+    embed = discord.Embed(
+        description=member.mention + "** está joya**",
+        color=discord.Colour.random(),
+    )
+    embed.set_image(
+        url=
+        "https://i.pinimg.com/originals/31/6b/3c/316b3cd3d0c2522a3c9580cc74ba6ea7.jpg"
+    )
+    embed.set_footer(text=foot)
+    await ctx.send(embed=embed)
+
+
+@joya.error
+async def joya_error(ctx, error):
+    await ctx.channel.purge(limit=1)
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(
+            f"Necesitas introducir el usuario que está joya {ctx.author.mention}"
+        )
+
+
+# Public
+# info
+@client.command()
+async def info(ctx):
+    await ctx.channel.purge(limit=1)
+    embed = discord.Embed(title=f"**{ctx.guild.name}**",
+                          description=f"**Owner:** {ctx.guild.owner}"
+                          f"**Miembros:** {ctx.guild.member_count}"
+                          f"\n**Servidor creado el: "
+                          f"\n**{ctx.guild.created_at}\n",
+                          timestamp=datetime.datetime.utcnow(),
+                          color=discord.Colour.green())
+    embed.set_footer(text=foot)
+    embed.set_thumbnail(url=f"{ctx.guild.icon_url}")
+    await ctx.send(embed=embed)
+
+
+# random
+@client.command()
+async def random10(ctx):
+    await ctx.channel.purge(limit=1)
+    embed = discord.Embed(title="**Número al azar del 0 al 10**",
+                          description=f"__El bot ha elegido:__"
+                          f"\n`{random.randint(0, 11)}`",
+                          timestamp=datetime.datetime.utcnow(),
+                          color=discord.Colour.blurple())
+    embed.set_thumbnail(url="https://pngimg.com/uploads/dice/dice_PNG49.png")
+    embed.set_footer(text=foot)
+    await ctx.send(embed=embed)
+
+
+@client.command()
+async def random100(ctx):
+    await ctx.channel.purge(limit=1)
+    embed = discord.Embed(title="**Número al azar del 0 al 100**",
+                          description=f"__El bot ha elegido:__"
+                          f"\n`{random.randint(0, 101)}`",
+                          timestamp=datetime.datetime.utcnow(),
+                          color=discord.Colour.blurple())
+    embed.set_thumbnail(url="https://pngimg.com/uploads/dice/dice_PNG49.png")
+    embed.set_footer(text=foot)
+    await ctx.send(embed=embed)
+
+
+# help
+@client.command()
+async def help(ctx):
+    await ctx.channel.purge(limit=1)
+    embed = discord.Embed(
+        title="**###𝐇𝐄𝐋𝐏###**",
+        description=
+        "**Comandos**\n.help (Todos los comandos en el servidor)"
+        "\n.info (Información del servidor)\n.ppt (Piedra, Papel, Tijeras)"
+        "\n.random10 (Número al azar del 0 al 10)\n.random100 (Número al azar del 0 al 100)"
+        "\n.repe (El bot repite lo que escribas)\n.mongollland (Invitación para MongollLand)",
+        timestamp=datetime.datetime.utcnow(),
+        color=discord.Colour.dark_blue())
+    embed.add_field(
+        name="**Info extra**",
+        value=">Bot creado por Tupei#9748\n"
+        ">Disfrutad el bot!!!\n>Hay comandos secretos escondidos en el bot!\n"
+              ">Si quieres una versión mejoradao la versión para moderar de este bot, contacta con Tupei",
+        inline=True)
+    embed.set_thumbnail(url=f"{ctx.guild.icon_url}")
+    embed.set_footer(text="Creado por Tupei con amor")
+    await ctx.send(embed=embed)
+
+
+# ppt
+@client.command()
+async def ppt(ctx, *, args):
+    await ctx.channel.purge(limit=1)
+    var = ["`Piedra`", "`Papel`", "`Tijeras`"]
+    embed = discord.Embed(title="**Piedra, Papel, Tijeras**",
+                          description="El bot ha elegido: " +
+                          random.choice(var) + "\nTú has elegido: " + "`" +
+                          args + "`",
+                          color=discord.Colour.blurple(),
+                          timestamp=datetime.datetime.utcnow())
+    embed.set_author(
+        name=f"{ctx.author.name}"
+    )
+    embed.set_thumbnail(
+        url=
+        "https://e7.pngegg.com/pngimages/243/115/png-clipart-rock-paper-scissors-scissors-stone.png"
+    )
+    embed.set_footer(text=foot)
+    await ctx.send(embed=embed)
+
+
+@ppt.error
+async def ppt_error(ctx, error):
+    await ctx.channel.purge(limit=1)
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(":x: Necesitas introducir un objeto con el que jugar")
+
+
+# repetidor
+@client.command()
+async def repe(ctx, *, args):
+    await ctx.channel.purge(limit=1)
+    await ctx.send(f"{ctx.author.mention}: " + args)
+
+
+@repe.error
+async def repe_error(ctx, error):
+    await ctx.channel.purge(limit=1)
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(":x: Necesitas introducir un mensaje")
+
+
+# mongollland
+@client.command()
+async def mongollland(ctx):
+    await ctx.channel.purge(limit=1)
+    embed = discord.Embed(title=f"**Invitación para MongollLand**",
+                          description="\nhttps://discord.gg/UcW5cc37Ad",
+                          timestamp=datetime.datetime.utcnow(),
+                          color=discord.Colour.dark_green())
+    embed.set_footer(text=foot)
+    embed.set_image(url="https://cdn.discordapp.com/icons/762689124855447572/a_6766e912c05dbc60db79e8437727edd5.webp?size=128")
+    await ctx.send(embed=embed)
+
+# Music
+# join
+# leave
+# play
+# next
+# stop
+# resume
+# BackUp
+client.run(TOKEN)
